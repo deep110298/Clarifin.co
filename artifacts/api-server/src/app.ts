8b9a51db8bc373b26f56659663c3761703ce9469
Deep shah <deepshah@macbook-air-2.mynetworksettings.com>
2026-04-03 15:39:04 -0400

    Fix Express 5 wildcard route

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -40,7 +40,7 @@ app.use("/api", router);
 // Serve frontend static files
 const frontendDist = path.resolve(__dirname, "../../clarifin/dist/public");
 app.use(express.static(frontendDist));
-app.get("*", (_req, res) => {
+app.get("/{*path}", (_req, res) => {
   res.sendFile(path.join(frontendDist, "index.html"));
 });
 
